57aa09b3dc11ddba01d6a242ae515f647172de45
梁灏 <admin@aresn.com>
2017-09-12 13:56:17 +0800

    release 1.0.0-beta.3

--- example/example.vue
+++ example/example.vue
@@ -5,6 +5,17 @@
             <i-option value="shanghai">上海</i-option>
         </i-select>
         <Switch></Switch>
+        <Circle></Circle>
+        <i-menu :active-name="activeName">
+            <i-menu-item name="1"></i-menu-item>
+        </i-menu>
+        <i-dropdown>
+            <i-dropdown-item></i-dropdown-item>
+        </i-dropdown>
+        <i-radio-group v-model="value">
+            <i-radio name="1"></i-radio>
+            <i-radio name="2"></i-radio>
+        </i-radio-group>
     </div>
 </template>
 <script>
